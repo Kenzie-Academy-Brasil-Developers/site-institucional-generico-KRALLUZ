b338d14013f9816b37c481c5d6fec1bb832ed619
Carlos_klz <chenrique201520@gmail.com>
2023-05-27 21:08:54 -0300

    envio

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,21 @@
-/* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+function showModalButton(){
+    const button = document.querySelector(".showModal");
+    const modalContainer = document.querySelector(".modalContainer");
+
+    button.addEventListener("click", () =>{
+        modalContainer.showModal();
+
+        closeModalButton();
+    });
+}
+
+function closeModalButton(){
+    const button = document.querySelector(".closeModal");
+    const modalContainer = document.querySelector(".modalContainer");
+
+    button.addEventListener("click", () =>{
+        modalContainer.close();
+    });
+}
+
+showModalButton();
